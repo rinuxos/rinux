e53aafb94868bcf3ff02175fb9c07273a1fe96b1
Hypernite <drunkenmonkey4987@gmail.com>
2022-08-02 19:14:09 -0400

    Added Funding

--- rinux/src/conf/mod.rs
+++ rinux/src/conf/mod.rs
@@ -1,3 +1,3 @@
-#[allow(dead_code)] pub(crate) const PROJECT_NAME: &'static str = "";
-#[allow(dead_code)] pub(crate) const PROJECT_VERSION: &'static str = "";
+#[allow(dead_code)] pub(crate) const PROJECT_NAME: &'static str = "MyProject";
+#[allow(dead_code)] pub(crate) const PROJECT_VERSION: &'static str = "v0.1.0BETA";
 #[allow(dead_code)] pub(crate) const QUIET_BOOT: bool = false;
